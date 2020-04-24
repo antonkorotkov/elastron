bd8131f7ffb3611cbc49ba879282ebb5bf1ba01b
Anton <antonkorotkoff@gmail.com>
2020-04-24 20:19:58 +0300

    pagination

---
+++ src/components/pagination/Pagination.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const trigger = createEventDispatcher()
+
+  export let current_page = 0
+  export let total_items = 0
+  export let items_per_page = 10
+  export let disable = false
+  export let offset = 0
+  export let className = ''
+
+  $: page = current_page + 1
+  $: total_pages = total_items > 0 ? Math.ceil(total_items / items_per_page) : 0
+  $: prevDisabled = disable || current_page == 0
+  $: nextDisabled = disable || current_page == total_pages - 1
+
+  $: shouldDisplay =
+    total_items > items_per_page &&
+    !isNaN(current_page) &&
+    page <= total_pages &&
+    offset % items_per_page == 0
+
+  const onClickPrev = event => {
+    if (prevDisabled) return
+    trigger('prev', --current_page)
+  }
+
+  const onClickNext = event => {
+    if (nextDisabled) return
+    trigger('next', ++current_page)
+  }
+</script>
+
+{#if shouldDisplay}
+  Page {page} of {total_pages}
+  <div class="ui pagination menu {className}">
+    <a
+      class="icon item"
+      class:disabled={prevDisabled}
+      on:click={onClickPrev}
+      href="javascript:;">
+      <i class="left chevron icon" />
+    </a>
+    <a
+      class="icon item"
+      class:disabled={nextDisabled}
+      on:click={onClickNext}
+      href="javascript:;">
+      <i class="right chevron icon" />
+    </a>
+  </div>
+{/if}
--- src/workspace/search/Search.svelte
+++ src/workspace/search/Search.svelte
@@ -4,6 +4,8 @@
   import { useStoreon } from '@storeon/svelte'
   import { onMount, onDestroy, afterUpdate } from 'svelte'
 
+  import Pagination from '../../components/pagination/Pagination.svelte'
+
   import 'jsoneditor/dist/jsoneditor.min.css'
 
   const { dispatch, search, indices } = useStoreon('search', 'indices')
@@ -11,24 +13,28 @@
   let requestBodyEditor, resultsEditor
   let qEditor, rEditor
 
-  $:  _indices = $indices.data.map(item => 
-        item[$indices.columns.reduce((i, item, index) => 
-          item === 'index' ? index : i, 0
-        )]
-      )
+  $: _indices = $indices.data.map(
+    item =>
+      item[
+        $indices.columns.reduce(
+          (i, item, index) => (item === 'index' ? index : i),
+          0
+        )
+      ]
+  )
 
   const onEditorChange = () => {
     try {
       if (qEditor) {
         const requestBody = qEditor.get()
-        dispatch('search/update', {requestBody})
+        dispatch('search/update', { requestBody })
       }
     } catch (err) {}
   }
 
   const onDocTypeChange = e => {
     const docType = e.target.value.trim() || '_doc'
-    dispatch('search/update', {docType})
+    dispatch('search/update', { docType })
     e.target.value = docType
   }
 
@@ -39,54 +45,68 @@
   const onStateFieldChange = data => dispatch('search/update', data)
 
   const onSizeChange = e => {
-    const size = !e.target.value.trim() || Number(e.target.value.trim()) < 0 ? 10 : Number(e.target.value.trim())
-    dispatch('search/update', {size})
+    const size =
+      !e.target.value.trim() || Number(e.target.value.trim()) < 0
+        ? 10
+        : Number(e.target.value.trim())
+    dispatch('search/update', { size })
     e.target.value = size
   }
 
   const onFromChange = e => {
-    const from = !e.target.value.trim() || Number(e.target.value.trim()) < 0 ? 0 : Number(e.target.value.trim())
-    dispatch('search/update', {from})
+    const from =
+      !e.target.value.trim() || Number(e.target.value.trim()) < 0
+        ? 0
+        : Number(e.target.value.trim())
+    dispatch('search/update', { from })
     e.target.value = from
   }
 
   onMount(() => {
     if (requestBodyEditor) {
-      qEditor = new JSONEditor(requestBodyEditor, {
-        mode: 'code',
-        onChange: onEditorChange
-      }, $search.requestBody)
-      qEditor.aceEditor.setOptions({maxLines: 32})
+      qEditor = new JSONEditor(
+        requestBodyEditor,
+        {
+          mode: 'code',
+          onChange: onEditorChange,
+        },
+        $search.requestBody
+      )
+      qEditor.aceEditor.setOptions({ maxLines: 32 })
     }
 
     if (resultsEditor) {
-      rEditor = new JSONEditor(resultsEditor, {
-        mode: 'tree',
-        onEditable: () => false,
-        onCreateMenu: (items, node) => {
-          if (node.type === 'single' && node.path.length === 1) {
-            return [
-              {
-                text: 'Edit',
-                title: 'Edit the document and commit updates to the server',
-                className: 'jsoneditor-type-object',
-                click: () => {
-                  alert('Comming soon')
-                }
-              },
-              {
-                text: 'Delete',
-                title: 'Delete the document from the server',
-                className: 'jsoneditor-remove',
-                click: () => {
-                  alert('Comming soon')
-                }
-              }
-            ]
-          }
-          return []
-        }
-      }, $search.results)
+      rEditor = new JSONEditor(
+        resultsEditor,
+        {
+          mode: 'tree',
+          onEditable: () => false,
+          onCreateMenu: (items, node) => {
+            if (node.type === 'single' && node.path.length === 1) {
+              return [
+                {
+                  text: 'Edit',
+                  title: 'Edit the document and commit updates to the server',
+                  className: 'jsoneditor-type-object',
+                  click: () => {
+                    alert('Comming soon')
+                  },
+                },
+                {
+                  text: 'Delete',
+                  title: 'Delete the document from the server',
+                  className: 'jsoneditor-remove',
+                  click: () => {
+                    alert('Comming soon')
+                  },
+                },
+              ]
+            }
+            return []
+          },
+        },
+        $search.results
+      )
     }
   })
 
@@ -104,6 +124,11 @@
       rEditor.destroy()
     }
   })
+
+  const onPaginationChanged = ({ detail }) => {
+    dispatch('search/update', { from: $search.size * detail })
+    dispatch('search/run')
+  }
 </script>
 
 <style>
@@ -111,12 +136,16 @@
     display: none;
   }
 
-  .search-options input[type="number"] {
+  .search-options input[type='number'] {
     width: 6rem !important;
   }
 
   .stats {
     margin-bottom: 7px;
+  }
+
+  .pagination {
+    text-align: right;
   }
 </style>
 
@@ -126,18 +155,26 @@
       <div class="fields search-options">
         <div class="field">
           <label for="type">Search Type</label>
-          <select id="type" class="ui dropdown" on:change={e => onStateFieldChange({type:e.target.value})} value={$search.type}>
+          <select
+            id="type"
+            class="ui dropdown"
+            on:change={e => onStateFieldChange({ type: e.target.value })}
+            value={$search.type}>
             <option value="uri">URI Search</option>
             <option value="body">Request Body</option>
           </select>
         </div>
         <div class="field">
           <label for="index">Index</label>
-          <select id="index" class="ui dropdown" on:change={e => onStateFieldChange({index:e.target.value})} value={$search.index}>
+          <select
+            id="index"
+            class="ui dropdown"
+            on:change={e => onStateFieldChange({ index: e.target.value })}
+            value={$search.index}>
             <option value="_all">All</option>
             {#if _indices.length}
               {#each _indices as index}
-                <option value="{index}">{index}</option>
+                <option value={index}>{index}</option>
               {/each}
             {/if}
           </select>
@@ -146,28 +183,40 @@
         {#if $search.type === 'uri'}
           <div class="field">
             <label for="size">Size</label>
-            <input type="number" id="size" 
-              on:change={onSizeChange} 
+            <input
+              type="number"
+              id="size"
+              on:change={onSizeChange}
               value={$search.size} />
           </div>
           <div class="field">
             <label for="from">From</label>
-            <input type="number" id="from" 
-              on:change={onFromChange} 
+            <input
+              type="number"
+              id="from"
+              on:change={onFromChange}
               value={$search.from} />
           </div>
           <div class="field">
             <label for="sort">Sort</label>
-            <input type="text" id="sort" 
-              on:change={e => onStateFieldChange({sort: e.target.value.trim()})} 
+            <input
+              type="text"
+              id="sort"
+              on:change={e => onStateFieldChange({
+                  sort: e.target.value.trim(),
+                })}
               value={$search.sort} />
           </div>
 
           <div class="field">
             <label>_Source</label>
             <div class="ui checkbox">
-              <input id="source" type="checkbox" 
-                on:change={e => onStateFieldChange({useSource: e.target.checked})} 
+              <input
+                id="source"
+                type="checkbox"
+                on:change={e => onStateFieldChange({
+                    useSource: e.target.checked,
+                  })}
                 checked={$search.useSource} />
               <label for="source">Enable</label>
             </div>
@@ -176,18 +225,24 @@
           {#if $search.useSource}
             <div class="field">
               <label for="source-value">&nbsp;</label>
-              <input type="text" id="source-value" 
-                on:change={e => onStateFieldChange({_source: e.target.value})}
+              <input
+                type="text"
+                id="source-value"
+                on:change={e => onStateFieldChange({ _source: e.target.value })}
                 value={$search._source} />
             </div>
           {/if}
-        {/if} 
+        {/if}
 
         <div class="field">
           <label>Doc Type</label>
           <div class="ui checkbox">
-            <input id="use-doc-type" type="checkbox" 
-              on:change={e => onStateFieldChange({useDocType: e.target.checked})} 
+            <input
+              id="use-doc-type"
+              type="checkbox"
+              on:change={e => onStateFieldChange({
+                  useDocType: e.target.checked,
+                })}
               checked={$search.useDocType} />
             <label for="use-doc-type">Enable</label>
           </div>
@@ -196,19 +251,22 @@
         {#if $search.useDocType}
           <div class="field">
             <label for="type-value">&nbsp;</label>
-            <input type="text" id="type-value" 
+            <input
+              type="text"
+              id="type-value"
               on:change={onDocTypeChange}
               value={$search.docType} />
           </div>
         {/if}
       </div>
       <div class="field" class:hidden={$search.type !== 'body'}>
-        <div id="request-body-editor" bind:this={requestBodyEditor}></div>
+        <div id="request-body-editor" bind:this={requestBodyEditor} />
       </div>
       {#if $search.type === 'body'}
-        <button class="ui green button" 
-          class:loading={$search.loading} 
-          disabled={$search.loading} 
+        <button
+          class="ui green button"
+          class:loading={$search.loading}
+          disabled={$search.loading}
           on:click={onSearchRun}>
           Run
         </button>
@@ -216,10 +274,15 @@
       <div class="field" class:hidden={$search.type !== 'uri'}>
         <label for="uri">URI Query</label>
         <div class="ui fluid action input">
-          <input id="uri" type="text" on:change={e => onStateFieldChange({uriQuery:e.target.value})} value={$search.uriQuery} />
-          <button class="ui green button" 
-            class:loading={$search.loading} 
-            disabled={$search.loading} 
+          <input
+            id="uri"
+            type="text"
+            on:change={e => onStateFieldChange({ uriQuery: e.target.value })}
+            value={$search.uriQuery} />
+          <button
+            class="ui green button"
+            class:loading={$search.loading}
+            disabled={$search.loading}
             on:click={onSearchRun}>
             Run
           </button>
@@ -229,14 +292,41 @@
   </div>
 
   <div class="ui segment">
-    <div class="ui circular labels stats">
-      Documents found: &nbsp;<span class="ui label">{$search.stats.total_results}</span>
-      Time: &nbsp;<span class="ui label">{$search.stats.time/1000}s</span>
-      Shards: &nbsp;<span class="ui blue label" title="Total">{$search.stats.total_shards}</span>
-      <span class="ui green label" title="Successful">{$search.stats.successful_shards}</span>
-      <span class="ui yellow label" title="Skipped">{$search.stats.skipped_shards}</span>
-      <span class="ui red label" title="Failed">{$search.stats.failed_shards}</span>
+    <div class="ui grid">
+      <div class="twelve wide column">
+        <div class="ui circular labels stats">
+          Documents found: &nbsp;
+          <span class="ui label">{$search.stats.total_results}</span>
+          Time: &nbsp;
+          <span class="ui label">{$search.stats.time / 1000}s</span>
+          Shards: &nbsp;
+          <span class="ui blue label" title="Total">
+            {$search.stats.total_shards}
+          </span>
+          <span class="ui green label" title="Successful">
+            {$search.stats.successful_shards}
+          </span>
+          <span class="ui yellow label" title="Skipped">
+            {$search.stats.skipped_shards}
+          </span>
+          <span class="ui red label" title="Failed">
+            {$search.stats.failed_shards}
+          </span>
+        </div>
+      </div>
+      <div class="four wide column pagination">
+        <Pagination
+          className="mini"
+          disable={$search.loading}
+          current_page={Math.round($search.from / $search.size)}
+          offset={$search.from}
+          items_per_page={$search.size}
+          total_items={$search.stats.total_results}
+          on:prev={onPaginationChanged}
+          on:next={onPaginationChanged} />
+      </div>
     </div>
-    <div id="results-editor" bind:this={resultsEditor}></div>
+
+    <div id="results-editor" bind:this={resultsEditor} />
   </div>
 </div>
