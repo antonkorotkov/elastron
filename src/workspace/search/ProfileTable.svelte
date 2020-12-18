a4b762546662e016fc10eab225a91ca4d3599a7e
Anton Korotkov <antonkorotkoff@gmail.com>
2020-12-18 18:46:28 +0200

    wip profile

--- src/workspace/search/ProfileTable.svelte
+++ src/workspace/search/ProfileTable.svelte
@@ -1,3 +1,21 @@
 <script>
+  import isEmpty from 'lodash/isEmpty'
+  import { useStoreon } from '@storeon/svelte'
 
+  import Shard from './profiling/Shard.svelte'
+
+  const { dispatch, search } = useStoreon('search')
 </script>
+
+<div class="profile-table">
+  {#if !isEmpty($search.profile)}
+    <h4 class="ui header">Affected Shards</h4>
+    <div class="ui styled fluid accordion">
+      {#each $search.profile.shards as shard, i}
+        <div class="profile-table__shard">
+          <Shard {shard} />
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
---
+++ src/workspace/search/profiling/Shard.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let shard
+
+  let active = false
+</script>
+
+<div class="title" class:active on:click={() => (active = !active)}>
+  <i class="dropdown icon" />
+  {shard.id}
+</div>
+<div class="content" class:active>test</div>
