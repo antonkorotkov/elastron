8dd717bb95ba2c76d3ca223590852d908f0588f7
Anton Korotkov <antonkorotkoff@gmail.com>
2025-05-02 21:29:20 +0200

    reformat

--- src/store/elasticsearch/indices.js
+++ src/store/elasticsearch/indices.js
@@ -1,68 +1,68 @@
 import API from '../../api/elasticsearch'
 
 export const indices = store => {
-    store.on('@init', () => ({
-        indices: {
-            columns: [],
-            data: [],
-            loading: false,
-            search: '',
+	store.on('@init', () => ({
+		indices: {
+			columns: [],
+			data: [],
+			loading: false,
+			search: '',
 			sorting: [] // [ direction, column, index ]
-        },
-    }))
+		},
+	}))
 
-    store.on('connected', () => {
-        store.dispatch('elasticsearch/indices/fetch')
-    })
+	store.on('connected', () => {
+		store.dispatch('elasticsearch/indices/fetch')
+	})
 
-    store.on('disconnected', () => {
-        store.dispatch('elasticsearch/indices/update', {
-            columns: [],
-            data: [],
-            loading: false,
+	store.on('disconnected', () => {
+		store.dispatch('elasticsearch/indices/update', {
+			columns: [],
+			data: [],
+			loading: false,
 			search: '',
 			sorting: []
-        })
-    })
+		})
+	})
 
-    store.on('elasticsearch/indices/fetch', async (state, cb) => {
-        try {
-            store.dispatch('elasticsearch/indices/update', {
-                loading: true,
-            })
-            const api = new API(state.connection)
-            const indices = await api.getIndices()
-            if (indices) {
-                const { columns, data } = indices
-                store.dispatch('elasticsearch/indices/update', {
-                    columns,
-                    data,
-                    loading: false,
-                })
-                if (typeof cb === 'function') {
-                    cb()
-                }
-            } else {
-                store.dispatch('notification/add', {
-                    type: 'error',
-                    message: 'Could not get indices data',
-                })
-                store.dispatch('elasticsearch/indices/update', {
-                    loading: false,
-                })
-            }
-        } catch (error) {
-            store.dispatch('notification/add', {
-                type: 'error',
-                message: error.message,
-            })
-            store.dispatch('elasticsearch/indices/update', {
-                loading: false,
-            })
-        }
-    })
+	store.on('elasticsearch/indices/fetch', async (state, cb) => {
+		try {
+			store.dispatch('elasticsearch/indices/update', {
+				loading: true,
+			})
+			const api = new API(state.connection)
+			const indices = await api.getIndices()
+			if (indices) {
+				const { columns, data } = indices
+				store.dispatch('elasticsearch/indices/update', {
+					columns,
+					data,
+					loading: false,
+				})
+				if (typeof cb === 'function') {
+					cb()
+				}
+			} else {
+				store.dispatch('notification/add', {
+					type: 'error',
+					message: 'Could not get indices data',
+				})
+				store.dispatch('elasticsearch/indices/update', {
+					loading: false,
+				})
+			}
+		} catch (error) {
+			store.dispatch('notification/add', {
+				type: 'error',
+				message: error.message,
+			})
+			store.dispatch('elasticsearch/indices/update', {
+				loading: false,
+			})
+		}
+	})
 
-    store.on('elasticsearch/indices/update', (state, indices) => ({
-        indices: { ...state.indices, ...indices },
-    }))
+	store.on('elasticsearch/indices/update', (state, indices) => ({
+		indices: { ...state.indices, ...indices },
+	}))
 }
