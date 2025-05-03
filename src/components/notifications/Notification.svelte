c1b8757481c9bcd8f934a18464f573abb3d305a0
Anton Korotkov <antonkorotkoff@gmail.com>
2025-05-03 22:29:28 +0200

    cleanup

--- src/components/notifications/Notification.svelte
+++ src/components/notifications/Notification.svelte
@@ -24,7 +24,8 @@
 	onMount(scheduleHide)
 </script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_noninteractive_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <div
 	onclick={() => dispatch('notification/delete', notification.id)}
 	onmouseenter={onNotificationMouseEnter}
