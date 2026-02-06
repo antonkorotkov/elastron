20b45e834e8f0675a5339d3f8f66364fa869b027
Anton Korotkov <antonkorotkoff@gmail.com>
2026-02-06 17:49:05 +0100

    rollup to vite

--- main.js
+++ main.js
@@ -9,98 +9,102 @@ import electronRemoteMain from '@electron/remote/main/index.js'
 electronRemoteMain.initialize()
 
 const createWindow = () => {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		width: 1440,
-		height: 960,
-		minWidth: 1280,
-		minHeight: 768,
-		titleBarStyle: 'hiddenInset',
-		show: true,
-		backgroundColor: '#000',
-		webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true,
-			contextIsolation: false,
-			nativeWindowOpen: true,
-			devTools: true
-		},
-	})
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1440,
+        height: 960,
+        minWidth: 1280,
+        minHeight: 768,
+        titleBarStyle: 'hiddenInset',
+        show: true,
+        backgroundColor: '#000',
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false,
+            nativeWindowOpen: true,
+            devTools: true
+        },
+    })
 
-	// and load the index.html of the app.
-	mainWindow.loadFile('public/index.html')
+    // and load the index.html of the app.
+    if (process.env.VITE_DEV_SERVER_URL) {
+        mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL)
+    } else {
+        mainWindow.loadFile('dist/index.html')
+    }
 
-	mainWindow.webContents.on('new-window', function (e, url) {
-		e.preventDefault()
-		shell.openExternal(url)
-	})
+    mainWindow.webContents.on('new-window', function (e, url) {
+        e.preventDefault()
+        shell.openExternal(url)
+    })
 
-	Menu.setApplicationMenu(
-		Menu.buildFromTemplate([
-			{
-				label: app.name,
-				submenu: [{ role: 'about' }, { role: 'quit' }],
-			},
-			{
-				label: 'Edit',
-				submenu: [
-					{ role: 'undo' },
-					{ role: 'redo' },
-					{ type: 'separator' },
-					{ role: 'cut' },
-					{ role: 'copy' },
-					{ role: 'paste' },
-					{ role: 'selectAll' },
-				],
-			},
-			{
-				role: 'help',
-				submenu: [
-					{
-						label: 'Learn More',
-						click: async () => {
-							await shell.openExternal('https://elastron.eney.solutions')
-						},
-					},
-					{
-						label: 'Check For Updates',
-						click: () => updater.checkForUpdates(true),
-					},
-					{
-						label: 'Debug',
-						click: () => {
-							mainWindow.webContents.openDevTools()
-						}
-					}
-				],
-			},
-		])
-	)
+    Menu.setApplicationMenu(
+        Menu.buildFromTemplate([
+            {
+                label: app.name,
+                submenu: [{ role: 'about' }, { role: 'quit' }],
+            },
+            {
+                label: 'Edit',
+                submenu: [
+                    { role: 'undo' },
+                    { role: 'redo' },
+                    { type: 'separator' },
+                    { role: 'cut' },
+                    { role: 'copy' },
+                    { role: 'paste' },
+                    { role: 'selectAll' },
+                ],
+            },
+            {
+                role: 'help',
+                submenu: [
+                    {
+                        label: 'Learn More',
+                        click: async () => {
+                            await shell.openExternal('https://elastron.eney.solutions')
+                        },
+                    },
+                    {
+                        label: 'Check For Updates',
+                        click: () => updater.checkForUpdates(true),
+                    },
+                    {
+                        label: 'Debug',
+                        click: () => {
+                            mainWindow.webContents.openDevTools()
+                        }
+                    }
+                ],
+            },
+        ])
+    )
 
-	return mainWindow
+    return mainWindow
 }
 
 app.whenReady().then(() => {
-	const window = createWindow()
-	const messaging = messenger(window)
+    const window = createWindow()
+    const messaging = messenger(window)
 
-	electronRemoteMain.enable(window.webContents)
+    electronRemoteMain.enable(window.webContents)
 
-	updater.init(window)
-	updater.checkForUpdates()
+    updater.init(window)
+    updater.checkForUpdates()
 
-	messaging.listen('header-doubleclick', () => {
-		if (window.isMaximized()) {
-			window.setSize(1280, 768, false)
-			return window.center()
-		}
-		return window.maximize()
-	})
+    messaging.listen('header-doubleclick', () => {
+        if (window.isMaximized()) {
+            window.setSize(1280, 768, false)
+            return window.center()
+        }
+        return window.maximize()
+    })
 
-	messaging.listen('check-for-updates', () => {
-		if (Math.floor(Math.random() * 10) > 7) updater.checkForUpdates()
-	})
+    messaging.listen('check-for-updates', () => {
+        if (Math.floor(Math.random() * 10) > 7) updater.checkForUpdates()
+    })
 
-	dumper.init(messaging, window)
-	elasticProxy.init(messaging)
+    dumper.init(messaging, window)
+    elasticProxy.init(messaging)
 })
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import electron from 'vite-plugin-electron/simple'
+
+
+export default defineConfig({
+	plugins: [
+		svelte(),
+		electron({
+			main: {
+				entry: 'main.js',
+				vite: {
+					build: {
+						rollupOptions: {
+							output: {
+								format: 'cjs',
+								entryFileNames: '[name].js',
+							},
+						},
+					},
+				},
+			},
+			renderer: {},
+		}),
+	],
+})
